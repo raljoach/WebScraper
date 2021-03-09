88d2ff74196fc0fb1266caa3a5e6ab308bc7952a
Ralph Joachim <raljoach@gmail.com>
2021-03-08 19:12:12 -0800

    redesign: introduction of AirbnbNavigator, AirbnbScraper repurposed

---
+++ Airbnb/Scraper/AirbnbNavigator.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableData;
+
+namespace Airbnb.Scraper
+{
+    public class AirbnbNavigator : IDisposable
+    {
+        private IWebDriver _driver = null;
+        private AirbnbScraper _scraper = null;
+                
+        public AirbnbNavigator(string url)
+        {
+            _driver = new ChromeDriver();
+
+            // navigate to url
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            _driver.Navigate().GoToUrl(url);            
+            wait.Until(webDriver => webDriver.FindElement(AirbnbSelector.HomePage).Displayed);
+
+            var html = _driver.PageSource;
+            _scraper = new AirbnbScraper(html);
+            
+        }
+
+        public List<TableRow> GetListings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NextPage()
+        {            
+            var pagination = _driver.FindElement(AirbnbSelector.Pagination);
+            var href = _scraper.FindNextPage();
+            bool nextPageExists = !string.IsNullOrWhiteSpace(href);
+            if (nextPageExists)
+            {
+                var nextPage = pagination.FindElement(AirbnbSelector.Anchor("href", href));
+                nextPage.Click();
+            }
+            return nextPageExists;
+        }
+        
+        public void Dispose()
+        {
+            if (_driver != null)
+            {
+                _driver.Dispose();
+                _driver = null;
+            }
+        }
+    }
+}
--- Airbnb/Tool/Program.cs
+++ Airbnb/Tool/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TableData;
 
 namespace Airbnb.ScraperTool
 {
@@ -11,12 +12,19 @@ namespace Airbnb.ScraperTool
     {
         static void Main(string[] args)
         {
-            var newport = "https://www.airbnb.com/s/Newport--OR--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=april&flexible_trip_dates%5B%5D=march&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Newport%2C%20OR%2C%20United%20States&place_id=ChIJjZh8TNvVwVQRcvv8YfFyH9Q&checkin=2021-04-04&checkout=2021-04-11&source=structured_search_input_header&search_type=autocomplete_click";
-            var urls = new List<string>() { newport };
-            var scraper = new AirbnbScraper(urls);
-            var table = scraper.Scrape();
+            var url = "https://www.airbnb.com/s/Newport--OR--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=april&flexible_trip_dates%5B%5D=march&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Newport%2C%20OR%2C%20United%20States&place_id=ChIJjZh8TNvVwVQRcvv8YfFyH9Q&checkin=2021-04-04&checkout=2021-04-11&source=structured_search_input_header&search_type=autocomplete_click";
+            var table = new Table();
+            using (var nav = new AirbnbNavigator(url))
+            {                
+                do
+                {
+                    var listings = nav.GetListings();
+                    table.AddRange(listings);                    
+                }
+                while (nav.NextPage());
+            }
             table.ExportCsv("newport.csv");
             Console.ReadLine();
-        }
+        }        
     }
 }
