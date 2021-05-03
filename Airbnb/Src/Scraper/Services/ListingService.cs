89bc2810b2ff98bb47a7b6199a66b67dff767248
Ralph Joachim <raljoach@gmail.com>
2021-05-02 21:36:05 -0700

    update service helper class with Post method unimplemented

--- Airbnb/Src/Scraper/Services/ListingService.cs
+++ Airbnb/Src/Scraper/Services/ListingService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Airbnb.Scraper.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,13 @@ namespace Airbnb.Scraper.Services
 {
     public class ListingService
     {
+        private const string hostPort = "localhost:8080";
+        private string baseUrl = $"https://{hostPort}/api/listings/";
+        private WebClient webClient = new WebClient();
+
+        public void Post(AirbnbListing listing)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
